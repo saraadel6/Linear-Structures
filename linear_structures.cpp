4cb1f764d1c5728b19878d09a303a3aae9c71894
Doha Tarek <Doha_Tarek155>
2023-04-24 12:20:50 +0200

    stack whith linked list

--- linear_structures.cpp
+++ linear_structures.cpp
@@ -1,12 +1,22 @@
 #include"linear_structures.h"
 int main(){
-    doublyLinkedList<int> d1;
-    d1.insertAtTail(1);
-    d1.insertAtTail(2);
-    d1.insertAtTail(3);
-    d1.insertAtTail(4);
-    d1.insertAtTail(5);
-    // d1.swap(0, 4);
-    d1.clear();
-    d1.forwardTraversal();
+    Stack<int> s1;
+    s1.push(1);
+    s1.push(2);
+    s1.push(3);
+    s1.push(4);
+    s1.push(5);
+    s1.push(6);
+    s1.push(7);
+    // s1.clear();
+    s1.print();
+    s1.print();
+    cout << s1.isEmpty();
+    // d1.insertAtTail(1);
+    // d1.insertAtTail(2);
+    // d1.insertAtTail(3);
+    // d1.insertAtTail(4);
+    // d1.insertAtTail(5);
+    // d1.clear();
+    // d1.forwardTraversal();
 }
--- linear_structures.h
+++ linear_structures.h
@@ -422,14 +422,99 @@ void doublyLinkedList<T>::clear(){
     length = 0;
 }
 
-// //1. create a temp node
-//   Node* temp = new Node();
-  
-//   //2. if the head is not null make temp as head and 
-//   //   move head to head next, then delete the temp,
-//   //   continue the process till head becomes null
-//   while(head != NULL) {
-//     temp = head;
-//     head = head->next;
-//     free(temp);
-//   }
+// --------------------------------------------------------------------------------------------------------------------------
+
+template<class T>
+class Stack{
+    struct Node{
+        T val;
+        Node* next;
+        Node(T val, Node* next){
+            this->val = val;
+            this->next = next;
+        }
+        Node(){}
+    };
+    Node* head;
+    int _size;
+public:
+    Stack();
+    void push(T newElement);
+    void pop();
+    T top();
+    bool isEmpty();
+    int stackSize();
+    void clear();
+    void print();
+};
+
+template <class T>
+Stack<T>::Stack(){
+    head = NULL;
+    _size = 0;
+}
+
+template <class T>
+bool Stack<T>::isEmpty(){
+    return _size;
+}
+
+template <class T>
+void Stack<T>::push(T newElement){
+    head = new Node(newElement, head);
+    ++_size;
+}
+template <class T>
+void Stack<T>::pop(){
+    if(_size == 0){
+        cout << "The stack is empty!\n";
+    } else {
+        Node* temp = head; 
+        head = head->next;
+        delete temp;
+        --_size;
+    }
+}
+
+template <class T>
+T Stack<T>::top(){
+    if(_size == 0){
+        cout << "The stack is empty!\n";
+        return 0;
+    } else {
+        return head->val;
+    }
+}
+
+template <class T>
+int Stack<T>::stackSize(){
+    return (_size == 0);
+}
+
+template <class T>
+void Stack<T>::clear(){
+    Node* temp = new Node();
+    while(head != NULL){
+        temp = head;
+        head = head->next;
+        delete temp;
+    }
+    _size = 0;
+}
+
+template <class T>
+void Stack<T>::print(){
+    if(head == NULL){
+    cout << "The stack is empty!\n";
+    } else{
+        Node* curr = head;
+        while(curr != NULL){
+            cout << curr->val << ' ';
+            curr = curr->next;
+        }
+        cout << '\n';
+    }
+}
+
+
+
